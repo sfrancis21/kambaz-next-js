d659874246664beb4ca7e73b8f710b231050388c
Sara Francis <sfmagoo333@gmail.com>
2025-09-12 12:25:54 -0400

    A1 Ready to Submit

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,6 +3,10 @@ export default function Labs() {
     return (
         <div id="wd-labs">
             <h1>Labs</h1>
+            <h2>Name: Sara Francis</h2><br/>
+            <h2>Section: CS5610 - 04</h2><br/>
+            <h2>Repo Link: <Link href="https://github.com/sfrancis21/kambaz-next-js" >Github Repo</Link><br/></h2>
+
             <ul>
                 <li>
                     <Link href="/Labs/Lab1" id="wd-lab1-link">
