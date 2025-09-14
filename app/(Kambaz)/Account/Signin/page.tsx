eb7b5ba6bca630972545ecf20867faaa8c4836d2
Sara Francis <sfmagoo333@gmail.com>
2025-09-14 18:24:42 -0400

    A1 Ready to Submit Part 6

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -5,7 +5,6 @@ export default function Signin() {
             <h3>Sign in</h3>
             <input placeholder="username" className="wd-username" /> <br />
             <input placeholder="password" type="password" className="wd-password" /> <br />
-            <Link id="wd-signin-btn" href="/Dashboard"> Sign in </Link> <br />
             <Link href="Profile" id="wd-signin-btn"> Sign in </Link> <br />
             <Link href="Signup" id="wd-signup-link"> Sign up </Link>
         </div>
